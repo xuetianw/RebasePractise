b9f73fbc5db76102e0ed83969b36078df6ee6e43
Fred <xuetian.wu@hcl.com>
2023-08-29 00:16:44 -0700

    fix testing branch

--- ucd/src/main/java/com/wupos/ucd/UcdApplication.java
+++ ucd/src/main/java/com/wupos/ucd/UcdApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UcdApplication {
 
 	public static void main(String[] args) {
+
+		// test
 		SpringApplication.run(UcdApplication.class, args);
 	}
 
